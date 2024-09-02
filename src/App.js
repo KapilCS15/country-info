e52aeeefc3ce2e7b989fcc670438dcd37e2fa090
KapilCS15 <saxenak737@gmail.com>
2024-09-02 19:13:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'; 
+import CountryInformation from './components/CountryInformation'; 
+  
+function App() { 
+  return ( 
+    <div className="App"> 
+      <CountryInformation /> 
+    </div> 
+  ); 
+} 
+  
+export default App; 
